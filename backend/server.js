17964c66d8c7bc34d23a93ae946dc372ac9f2dab
Benayah Wanyoike <wanyoikebenayah77@gmail.com>
2024-09-10 10:55:59 +0300

    setting up roytes in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
- const express =  require('express')
+const express = require('express');
 
-//  express app
+// express app
 const app = express();
+
+// routes
+app.get('/', (req, res) => {
+    res.json({ msg: "Welcome to the app" });
+});
 
 // listen for request 
 app.listen(4000, () => {
-    console.log('listening on port 4000')
-})
+    console.log('listening on port 4000');
+});
