bb0698729645d5d87fea1cd3c391930b2782b721
Benayah Wanyoike <wanyoikebenayah77@gmail.com>
2024-09-12 10:25:32 +0300

    server error

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express');
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workout')
 
 // express app
@@ -16,9 +17,16 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/workouts',workoutRoutes)
 
-// listen for request 
-app.listen(process.env.PORT, () => {
-    console.log('listening on port 4000', process.env.PORT);
-});
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+   .then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log('listening on port 4000', process.env.PORT);
+    });
+   })
+   .catch((error) =>{
+    console.log(error)
+   })
+
 
 process.env
