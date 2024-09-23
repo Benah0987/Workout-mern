f6ba22e06ff074e59d06b5b31b32a255a0d727ac
Benayah Wanyoike <wanyoikebenayah77@gmail.com>
2024-09-23 16:11:07 +0300

    home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react'
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+        <BrowserRouter>
+          <div className='pages'>
+            <Routes>
+                <Route  path = "/" element = {<Home />} />
+            </Routes>
+
+          </div>
+        </BrowserRouter>
+    </div>
   )
 }
 
