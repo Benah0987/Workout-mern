23f28e8b37005cec8a3f7734bfe4408aa9bf4748
Benayah Wanyoike <wanyoikebenayah77@gmail.com>
2024-09-23 16:21:20 +0300

    navbar, home pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,18 @@
-import {BrowserRouter, Routes, Route} from 'react'
-import Home from './pages/Home'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';  // react-router-dom, not just react!
+import Home from './pages/Home';  // Verify the correct path
 
 function App() {
   return (
     <div>
-        <BrowserRouter>
-          <div className='pages'>
-            <Routes>
-                <Route  path = "/" element = {<Home />} />
-            </Routes>
-
-          </div>
-        </BrowserRouter>
+      <BrowserRouter>
+        <div className='pages'>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
