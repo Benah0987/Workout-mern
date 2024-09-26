992b2b875e3a31cd7eb43110ffa30b0ffb2d24c2
Benayah Wanyoike <wanyoikebenayah77@gmail.com>
2024-09-26 16:04:47 +0300

    backend

--- backend/models/Workout.js
+++ backend/models/Workout.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose');
 
 const workoutSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  reps: {
-    type: Number,
-    required: true
-  },
-  load: {
-    type: Number,
-    required: true
-  }
-});
+    title: {
+        type: String,
+        required: true
+    },
+    reps: {
+        type: Number,
+        required: true
+    },
+    load: {
+        type: Number,
+        required: true
+    }
+}, { timestamps: true }); // This adds createdAt and updatedAt automatically
 
 module.exports = mongoose.model('Workout', workoutSchema);
