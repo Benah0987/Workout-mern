c56a1113eb3cc535133877779076b6e6254940f3
Benayah Wanyoike <wanyoikebenayah77@gmail.com>
2024-10-11 16:38:30 +0300

    changed model

--- backend/models/Workout.js
+++ backend/models/Workout.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const workoutSchema = new mongoose.Schema({
+    category: {
+        type: String,
+        required: true
+    },
     title: {
         type: String,
         required: true
